fc60af80f79cd5bb7aaf9fbb9c78e4d78af2a7ff
ikf3 <contact@ikf3.com>
2019-01-18 11:10:21 +0100

    WIP awstat
    Signed-off-by: ikf3 <contact@ikf3.com>

--- script.sh
+++ script.sh
@@ -28,7 +28,7 @@ updg="$upd && $upg"
 # Using apt-get instead of aptitude since that one doesn't have a stable cli
 silent='-qq -o Dpkg::Use-Pty=0'
 # Yep, this option is undocumented. We could pipe the output to null/zero but this is dirty work.However this doesn't work everytime
-locH="127.0.0.1     localhost wp.mywebchef.org"
+locH="127.0.0.1     localhost wp.mywebchef.org awstats.mywebchef.org server.mywebchef.org"
 
 login='talamo_a'
 mail='talamo_a@ikf3.com'
@@ -102,12 +102,32 @@ if ! [ -f /usr/bin/certbot ]; then
 fi
 echo -e "$Cyan$bold$sname$rs Installing required packages.. $Color_Off"
 $upd
-dlPkg "apache2 php7.0 php7.0-cli php7.0-common php7.0-opcache php7.0-curl php7.0-mbstring php7.0-mysql php7.0-zip php7.0-xml mysql-server libapache2-mod-php python virtualenv"
+dlPkg "apache2 php7.0 php7.0-cli php7.0-common php7.0-opcache php7.0-curl php7.0-mbstring php7.0-mysql php7.0-zip php7.0-xml mysql-server libapache2-mod-php awstats cron"
 }
 
 function wpInst () {
 echo -e "$Cyan$bold$sname$rs Updating System.. $Color_Off"
 $upd
+
+if ! grep -q "$locH" /etc/hosts ; then
+    echo -e "\n$locH" >> /etc/hosts
+fi
+
+if ! [ -d /etc/apache2/ssl ]; then
+    mkdir -p /etc/apache2/ssl
+    a2enmod ssl
+    openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/apache2/ssl/mywebchef.key -out /etc/apache2/ssl/mywebchef.crt -subj '/CN=localhost' -extensions EXT -config <( printf "[dn]\nCN=localhost\n[req]\ndistinguished_name = dn\n[EXT]\nsubjectAltName=DNS:localhost\nkeyUsage=digitalSignature\nextendedKeyUsage=serverAuth")
+    chmod 600 /etc/apache2/ssl/*
+fi
+
+if ! [ -f /etc/apache2/sites-available/myWebChef.org.conf ] ; then
+    echo -e "\n<VirtualHost *:80>\nServerName $www\nServerAlias $www\nDocumentRoot $WPPATH\nErrorLog \${APACHE_LOG_DIR}/error-wordpress.log\nCustomLog \${APACHE_LOG_DIR}/custom-wordpress.log combined\n</VirtualHost>\n<IfModule mod_ssl.c>\n<VirtualHost _default_:443>\nServerName $www:443\nServerAdmin talamo_a@ikf3.com\nDocumentRoot $WPPATH\nErrorLog \${APACHE_LOG_DIR}/error-wordpress.log\nCustomLog \${APACHE_LOG_DIR}/custom-wordpress.log combined\nSSLEngine on\nSSLCertificateFile      /etc/apache2/ssl/mywebchef.crt\nSSLCertificateKeyFile /etc/apache2/ssl/mywebchef.key\n<FilesMatch \"\.(cgi|shtml|phtml|php)$\">\nSSLOptions +StdEnvVars\n</FilesMatch>\n<Directory /usr/lib/cgi-bin>\nSSLOptions +StdEnvVars\n</Directory>\n</VirtualHost>\n</IfModule>" > /etc/apache2/sites-available/myWebChef.org.conf
+a2ensite myWebChef.org
+fi
+
+echo -e "\nServerName localhost" > /etc/apache2/conf-available/loc.conf
+a2enconf loc > /dev/null
+
 service mysql start
 mysql -u root -e "create database if not exists wordpress" > /dev/null 
 
@@ -119,6 +139,7 @@ if ! [ -f $WPPATH/wp-config.php ] ; then
     wp core config --dbname="wordpress" --dbuser="root" --path=$WPPATH --allow-root --quiet > /dev/null || { echo -e  "$Red$bold$sname$rs : There was an issue while connecting to SQL. Quitting."; log "E: SQL error on core config."; exit 1; }
     wp core install --allow-root --quiet --url=$www --title="$title" --admin_user="$login" --admin_password="$pass" --admin_email="$mail" --path=$WPPATH > /dev/null
     wp plugin install ${WPPLUGINS[@]} --activate --path=$WPPATH --quiet --allow-root > /dev/null
+    echo -e "$Green$bold$sname$rs Your credetials are the following:\nUser $login,\nPassword $pass,\nMail $mail,Adress $www\n"
 else
     wp core update --allow-root --quiet --path=$WPPATH
     echo -e "$Green$bold$sname$rs Wordpress has been updated!"
@@ -129,27 +150,62 @@ chown -R www-data:www-data $WPPATH
 find $WPPATH -type f -exec chmod 644 {} +
 find $WPPATH -type d -exec chmod 755 {} +
 
-if ! grep -q "$locH" /etc/hosts ; then
-    echo -e "\n$locH" >> /etc/hosts
+
+echo -e "$Cyan$sname$bold$rs \c"
+service apache2 restart > /dev/null
+if [ $? -ne 0 ]; then
+    echo "$Red$bold$sname$rs Fatal: There was an error while trying to configure wordpress. Please retry."
+    log "E: wp apache error, $?"
+         exit 1;
+     else
+        echo -e "$Green$bold$sname$rs Wordpress is now ready, check it out at wp.mywebchef.org $Color_Off"
 fi
 
-if ! [ -d /etc/apache2/ssl ]; then
-    a2enmod ssl
-    openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/apache2/ssl/mywebchef.key -out /etc/apache2/ssl/mywebchef.crt -subj '/CN=localhost' -extensions EXT -config <( printf "[dn]\nCN=localhost\n[req]\ndistinguished_name = dn\n[EXT]\nsubjectAltName=DNS:localhost\nkeyUsage=digitalSignature\nextendedKeyUsage=serverAuth")
-    chmod 600 /etc/apache2/ssl/*
-fi
 
-if ! [ -f /etc/apache2/sites-available/myWebChef.org.conf ] ; then
-    echo -e "\n<VirtualHost *:80>\nServerName $www\nServerAlias $www\nDocumentRoot $WPPATH\nErrorLog \${APACHE_LOG_DIR}/error-wordpress.log\nCustomLog \${APACHE_LOG_DIR}/custom-wordpress.log combined\n</VirtualHost>
-<IfModule mod_ssl.c>\n<VirtualHost _default_:443>\nServerName $www:443\nServerAdmin talamo_a@ikf3.com\nDocumentRoot $WPPATH\nErrorLog \${APACHE_LOG_DIR}/error-wordpress.log\nCustomLog \${APACHE_LOG_DIR}/custom-wordpress.log combined\nSSLEngine on\nSSLCertificateFile      /etc/apache2/ssl/mywebchef.crt\nSSLCertificateKeyFile /etc/apache2/ssl/mywebchef.key\n<FilesMatch \"\.(cgi|shtml|phtml|php)$\">\nSSLOptions +StdEnvVars\n</FilesMatch>\n<Directory /usr/lib/cgi-bin>\nSSLOptions +StdEnvVars\n</Directory>\n</VirtualHost>\n</IfModule>" > /etc/apache2/sites-available/myWebChef.org.conf
-fi
-echo -e "$Cyan$sname$bold$rs \c"
-a2ensite myWebChef.org
-echo -e "\nServerName localhost" > /etc/apache2/conf-available/loc.conf
-a2enconf loc > /dev/null
-echo -e "$Cyan$sname$bold$rs \c"
-service apache2 restart
-echo -e "$Green$bold$sname$rs Script is finished, your credetials are the following:\nUser $login,\nPassword $pass,\nMail $mail,Adress $www\n"
+}
+
+function awStat () {
+    echo -e "$Cyan$bold$sname$rs Installing awstats.. $Color_Off"
+    dlPkg "awstats"
+     if [ $? -ne 0 ]; then
+        echo "$Red$bold$sname$rs Fatal: could not get awstats. Please do it manually and relaunch the script."
+        log "E: awstats install error $?"
+    fi
+    if ! [ -d /var/www/awstats]; then
+        mkdir /var/www/awstats -p
+        ln -s /usr/share/awstats/icon /var/www/awstats/icon
+    fi
+    a2enmod authz_groupfile > /dev/null
+    if [ $? -ne 0 ]; then
+        echo "$Red$bold$sname$rs Fatal: could not get awstats. Please do it manually and relaunch the script."
+        log "E: awstats install error $?"
+        exit 1;
+    fi
+    if ! [ -f /etc/apache2/sites-available/awstats.mywebchef.org.conf ]; then
+        echo -e "" > /etc/apache2/sites-available/awstats.mywebchef.org.conf
+        a2ensite awstats.conf > /dev/null
+        if [ $? -ne 0 ]; then
+            echo "$Red$bold$sname$rs Fatal: There was an error while trying to apply awstats configuration. Please retry."
+            log "E: awstats apache conf error $?"
+            exit 1;
+        fi
+    fi
+    dlPkg "cron"
+     if [ $? -ne 0 ]; then
+        echo "$Red$bold$sname$rs Fatal: could not get awstats. Please do it manually and relaunch the script."
+        log "E: awstats install error $?"
+    else
+        crontab -l | { cat; echo -e "0 * * * * /usr/lib/cgi-bin/awstats.pl -config=apache -update\n5 * * * * /usr/lib/cgi-bin/awstats.pl -config=apache -output -staticlink > /var/www/awstats/index.html"; } | crontab -
+    fi 
+
+    service apache2 restart > /dev/null
+    if [ $? -ne 0 ]; then
+            echo "$Red$bold$sname$rs Fatal: There was an error while trying to configure awstats. Please retry."
+            log "E: awstats apache error $?"
+            exit 1;
+    else
+        echo -e "$Cyan$bold$sname$rs awstats is now installed, check it out at awstats.mywebchef.org $Color_Off"
+    fi
 }
 
 
